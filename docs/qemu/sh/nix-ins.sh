b214bc52bef448c3cc080b0f0f7a2aea14f4c360
xueshi hu <hubachelar@gmail.com>
2022-05-21 23:48:22 +0800

    :duck: tired

--- docs/qemu/sh/nix-ins.sh
+++ docs/qemu/sh/nix-ins.sh
@@ -7,3 +7,8 @@ mount /dev/disk/by-label/nixos /mnt
 nixos-generate-config --root /mnt
 
 export http_proxy=http://10.90.50.75:8889 && export https_proxy=http://10.90.50.75:8889
+
+# fileSystems."/" =
+#     { device = "/dev/disk/by-uuid/1142ac37-0fa1-44b6-9bc5-5fa034eb8862";
+#       fsType = "ext4";
+#     };
